b42667b13b4fa41e91760ca14213984db2b6a25d
Sreenath Gopinathan <sreenath4294@gmail.com>
2022-10-26 01:01:09 +0530

    exercises part 1

--- lib/pages/homescreen.dart
+++ lib/pages/homescreen.dart
@@ -13,172 +13,221 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[800],
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+      ]),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(
-            children: [
-              //greetings row
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                // ignore: prefer_const_literals_to_create_immutables
+        child: Column(
+          children: [
+            //greetings row
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 25.0, vertical: 12.00),
+              child: Column(
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        "Hi, Sree!",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            "Hi, Sree!",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "25 Oct, 2022",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
                       ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        "25 Oct, 2022",
-                        style: TextStyle(
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.blue[600],
+                            borderRadius: BorderRadius.circular(16)),
+                        padding: const EdgeInsets.all(12),
+                        child: const Icon(
+                          Icons.notifications,
                           color: Colors.white,
                         ),
                       )
                     ],
                   ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  // search bar
                   Container(
-                    decoration: BoxDecoration(
-                        color: Colors.blue[600],
-                        borderRadius: BorderRadius.circular(16)),
-                    padding: const EdgeInsets.all(12),
-                    child: const Icon(
-                      Icons.notifications,
-                      color: Colors.white,
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              // search bar
-              Container(
-                  decoration: BoxDecoration(
-                      color: Colors.blue[600],
-                      borderRadius: BorderRadius.circular(12)),
-                  padding: const EdgeInsets.all(12),
-                  child: Row(
+                      decoration: BoxDecoration(
+                          color: Colors.blue[600],
+                          borderRadius: BorderRadius.circular(12)),
+                      padding: const EdgeInsets.all(12),
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "Search",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      )),
+                  const SizedBox(
+                    height: 25,
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: const [
+                      Text(
+                        "How do you feel?",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
                       Icon(
-                        Icons.search,
+                        Icons.more_horiz,
                         color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        "Search",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
+                      )
                     ],
-                  )),
-              const SizedBox(
-                height: 25,
-              ),
+                  ),
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    "How do you feel?",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold),
+                  const SizedBox(
+                    height: 25,
                   ),
-                  Icon(
-                    Icons.more_horiz,
-                    color: Colors.white,
-                  )
-                ],
-              ),
 
-              const SizedBox(
-                height: 25,
-              ),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: const [
-                      Emoticons(
-                        emoticonFace: "😩",
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: const [
+                          Emoticons(
+                            emoticonFace: "😩",
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Bad",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
                       ),
-                      SizedBox(
-                        height: 8,
+                      Column(
+                        children: const [
+                          Emoticons(
+                            emoticonFace: "😊",
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Fine",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
                       ),
-                      Text(
-                        "Bad",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: const [
-                      Emoticons(
-                        emoticonFace: "😊",
+                      Column(
+                        children: const [
+                          Emoticons(
+                            emoticonFace: "😄",
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Well",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
                       ),
-                      SizedBox(
-                        height: 8,
+                      Column(
+                        children: const [
+                          Emoticons(
+                            emoticonFace: "🥳",
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Excellent",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
                       ),
-                      Text(
-                        "Fine",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: const [
-                      Emoticons(
-                        emoticonFace: "😄",
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        "Well",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: const [
-                      Emoticons(
-                        emoticonFace: "🥳",
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        "Excellent",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      )
                     ],
                   ),
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+            const SizedBox(
+              height: 13,
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(25),
+                color: Colors.grey[200],
+                child: Center(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text(
+                            "Exercises",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(Icons.more_horiz)
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16)),
+                        child: const ListTile(
+                          leading: Icon(Icons.favorite),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
